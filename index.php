0b796cb8805c63117cccdf378f448f3e10a428d0
Rebeka Meszaros <akeber.m@live.com>
2020-04-21 16:29:41 +0100

    simple PHP form submission with redirect function

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html> 
+<html lang="en"> 
+
+    <head> 
+
+        <meta charset="utf-8"> 
+
+        <title>Document</title> 
+
+        <link rel="stylesheet" href="css/styles.css?v=1.0"> 
+
+    </head> 
+
+    <body> 
+
+        <form action="name.php" method="post"> 
+
+            <label for="name">Your name</label> 
+
+            <input type="text" name="name" id="id"> 
+
+    
+            <button type="submit">Greet me!</button> 
+
+        </form> 
+
+    </body> 
+
+</html> 
---
+++ name.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = $_POST['name'] ?? null;
+
+if (empty(trim($name))) {
+
+    header('Location: http://localhost:8888/basics/index.php');
+}
+
+echo "Hi there, {$_POST['name']}!";
+
